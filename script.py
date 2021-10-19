328e3e3a4df557fb52bf3f1bf10cf9d2cb2d87b2
jsoles7 <45300260+jsoles7@users.noreply.github.com>
2021-10-19 00:06:28 -0400

    Iteration 2
    FIxed mistakes + smoothed out process

--- script.py
+++ script.py
@@ -4,34 +4,35 @@ import json
 import os
 import csv 
 
+
+def request_url(ticker, API_key):
+    """
+        This function is used to compile an accessible DeFi Pulse link using the ticker and API key as inputs.
+        @param: two parameters are passed to this function. The first is the ticker 
+                and is a string. The second is the API key which is a string.
+    """
+    link = f"https://public.defipulse.com/api/GetHistory?period=1y&{ticker}=all&format=csv&api-key={API_key}"
+    return link 
+
+
 #One simple main program
 
 if __name__ == "__main__":
     
     #get key
     API_KEY = os.environ.get("API_KEY")
-
-    #TVL URL
-    url = "https://public.defipulse.com/api/GetHistory?period=1y&project="
     
-    #End CSV component
-    end_url = "&format=csv&api-key="
     #Welcome message
     print("Welcome to the DeFi Pulse Data Reader")
     print("")
     print("Currently, this script only pulls TVL")
 
-    project_name = input("What project do you want to get TVL history for?")
+    project_ticker = str(input("What project do you want to get TVL history for? (Input ticker) "))
 
-    #url shaping
-    url_project = str(url, project_name)
-    url_with_csv = str(url_project, end_url)
-
-    url_to_go = str(url_with_csv, API_KEY)
-
+    url = request_url(project_ticker, API_KEY)
 
     #Grab data
-    response = requests.get(url_to_go)
+    response = requests.get(url)
 
 
 
